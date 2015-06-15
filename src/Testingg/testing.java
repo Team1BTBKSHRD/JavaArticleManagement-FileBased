1136c181449eadc14a1a4b07d2cf71f5be26bcba
kepisal <s0keangpisal>
2015-06-15 10:54:21 +0700

    this
    this is testing

--- src/Testingg/testing.java
+++ src/Testingg/testing.java
@@ -2,6 +2,7 @@ package Testingg;
 
 public class testing {
 	public static void main(String[] args) {
-		System.out.println("home111");
+		System.out.println("home12");
+		System.out.println("home13");
 	}
 }
