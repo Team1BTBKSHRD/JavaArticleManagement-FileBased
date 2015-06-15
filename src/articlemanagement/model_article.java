4077ec33d35acd98e846e3870c9b42371a71275a
kepisal <s0keangpisal>
2015-06-15 11:16:22 +0700

    comment
    add some comments.

---
+++ src/articlemanagement/model_article.java
@@ -0,0 +1,17 @@
+/**
+ * this is a article project management
+ */
+package articlemanagement;
+/**
+ * @author Ke Pisal, Hem Sarin, Chorn Elit, Rous Channa, Sun Vicheychetra, Sang Sambo
+ * 
+ *
+ */
+
+/*
+ * @import libraries
+ * */
+
+public class model_article {
+
+}
