eee412590385f2ae2eec7e34486fce2649426322
elit <chhornelit@gmail.com>
2015-06-20 08:45:06 +0700

    asdf

--- src/LogFile/LogFile.java
+++ src/LogFile/LogFile.java
@@ -1,12 +1,20 @@
-package LogFile;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.util.logging.*;
 
 public class LogFile {
+  public static Logger logger = null;
 	private static LogFile instance = null;
+  private Handler fileHandler;
 	private Path fileName;
 	protected LogFile() {
 		// Exists only to defeat instantiation.
 	}
+  public void setupLogger() throws IOException {
+      logger = Logger.getLogger("");
+      fileHandler = new FileHandler("log1.log", true);
+      logger.addHandler(fileHandler);
+  }
 	public static LogFile getInstance() {
 		if(instance == null)	instance = new LogFile();
 		return instance;
@@ -18,9 +26,15 @@ public class LogFile {
 		this.fileName = fileName;
 	}
 	
-	public void writeLogOpenningFile(){}
-	public void writeLogClosingFile(){}
-	public void writeLogModifingFile(){}
-	public void writeLogDeletingFile(){}
-	public void writeLogException(){}
+	public void writeLogOpenningFile(){
+    logger.setLevel(Level.ALL);
+    logger.severe("This is a SEVERE-level log");
+    logger.info("This is a info-level message");
+      fileHandler.flush();
+      fileHandler.close();
+  }
+//	public void writeLogClosingFile(){}
+//	public void writeLogModifingFile(){}
+//	public void writeLogDeletingFile(){}
+//	public void writeLogException(){}
 }
